15ad7955563b725ea717be869fe5c5ab9ffb9f77
Felix Komar <felix.komar@gmail.com>
2015-02-10 23:03:01 -0500

    quick fix fork

--- src/drillzone/stream_api_test/zeromq_test.cpp
+++ src/drillzone/stream_api_test/zeromq_test.cpp
@@ -5,10 +5,10 @@
 
 #include <assert.h>
 #include <string.h>
+
 #include "CppUnitTest.h"
 #include "CppUnitTestLogger.h"
 #include "CppUnitTestAssert.h"
-
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -16,7 +16,7 @@ namespace stream_api_test
 {		
 	TEST_CLASS(zeromq_test)
 	{
-	public:
+		public:
 		
 		TEST_METHOD(basic_test)
 		{
@@ -37,14 +37,39 @@ namespace stream_api_test
 			printf("Received Hello\n");
 			zmq_send(responder, "World", 5, 0);
 
-
 			int request_nbr;
 
 			printf("Sending Hello %d...\n", request_nbr);
 			zmq_send(requester, "Hello", 5, 0);
 			zmq_recv(requester, buffer, 10, 0);
 			printf("Received World %d\n", request_nbr);
-			
+
+			// Initialize poll set
+			zmq_pollitem_t items[] = 
+			{
+				{ responder, 0, ZMQ_POLLIN, 0 },
+				{ requester, 0, ZMQ_POLLIN, 0 }
+			};
+
+			zmq_msg_t message;
+
+			// Process messages from both sockets
+			while (1) {
+				zmq_poll(&items[0], 2, -1);
+
+				if (items[0].revents & ZMQ_POLLIN) 
+				{
+					responder.recv(&message);
+					// Process task
+				}
+
+				if (items[1].revents & ZMQ_POLLIN) 
+				{
+					subscriber.recv(&message);
+					// Process weather update
+				}
+			}
+
 			zmq_close(requester);
 			zmq_ctx_destroy(context);			
 		}
